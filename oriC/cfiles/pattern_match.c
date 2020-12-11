6725c693ee2d6a0d951014bd81b51b15a913a111
adypd97 <adypd1997@gmail.com>
2020-12-11 22:18:49 +0530

    crude version working

--- oriC/cfiles/pattern_match.c
+++ oriC/cfiles/pattern_match.c
@@ -3,24 +3,60 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <math.h>
+#include <limits.h>
 #include "pattern_match.h"
 #define safer_free(p) {free(p); (p) = NULL;}
 
 int main(int argc, char* argv[argc]){
 
-	char S[] = "ccaccaaegta";
-	//printf("%s\n", S);
+	char *S = "ccatgacttgaa";
 	char pattern[] = "atgacttga";
-	LONG hash_value = hash(pattern);
-	//printf("Hash value %u\n", hash_value);
+	size_t n = strlen(S);
+	size_t m = strlen(pattern);
+
+	LONG *start_locations = pattern_match(S, pattern, n, m);
+	if (start_locations) {
+		for(size_t i = 0; i < n-m+1; i++)
+			printf("%u \n", *(start_locations + i));
+		free(start_locations);
+	}
+
 	return 0;
 }
 
+LONG * pattern_match(char *T, char *p, size_t n , size_t m) {
+	LONG *start_locations = malloc(sizeof(LONG)*(n-m+1));
+	if (!start_locations)
+		return NULL;
+
+	LONG pattern_hash = hash(p); // pattern_hash;
+
+	char temp[m+1];
+	memset(temp, 0, sizeof(temp));
+	//LONG temp_hash = 0L;
+	int status = 1; // default is no match
+
+	for(size_t i = 0; i < n - m + 1; i++) { // how many time we move along text n - m
+		for(size_t j = i; j < i + m; j++) {
+			strncat(temp, &T[j], 1);
+			//printf("%d %d\n", i, j);
+		}
+		status = compare_hash(hash(temp), pattern_hash);
+		if (status == 0) {
+			start_locations[i] = (LONG)i;
+		}
+		memset(temp, 0, sizeof(temp));
+	}
+	return start_locations;
+}
+
+int compare_hash(LONG h1, LONG h2) {
+	return h1 == h2 ? 0 : 1; // 0 is success, 1 is failure of match
+}
 
 LONG hash(char *S) {
 	LONG hash_value = 0L;
 	size_t n = strlen(S);
-	printf("%s\n", S);
 	for(size_t i = 0; i < n; i++) {
 		if (S[i] == 'a')
 			ncode = a;
@@ -30,9 +66,9 @@ LONG hash(char *S) {
 			ncode = t;
 		else
 			ncode = g;
-		hash_value += ncode*((LONG) pow(4, n-1-i));
+		hash_value = (hash_value + ncode*((LONG) pow(4, n-1-i))) % UINT_MAX;
 	}
-
+	//static LONG prev_hash = hash_value;
 	return hash_value;
 }
 
--- oriC/cfiles/pattern_match.h
+++ oriC/cfiles/pattern_match.h
@@ -9,8 +9,11 @@ enum NCODE { a = 1, c = 2, g = 3, t = 4};
 enum NCODE ncode;
 
 LONG hash(char *S);  // hash function 
+LONG rolling_hash(char *S);  // hash function 
 
-LONG * rabin_karp(char *T, char *p);   // return pointer to array with starting locations
+int compare_hash(LONG h1, LONG h2); // compare hash from text segment(h1) to pattern hash h2
+
+LONG * pattern_match(char *T, char *p, size_t n, size_t m);   // return pointer to array with starting locations
 					 // of matches for p(pattern) in T (target/text)
 
 
