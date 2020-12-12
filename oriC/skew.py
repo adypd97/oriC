6e1b3465e9025182d892dd886a9a6bfb69222428
adypd97 <adypd1997@gmail.com>
2020-12-12 12:46:06 +0530

    refactor skew.py

--- oriC/skew.py
+++ oriC/skew.py
@@ -22,16 +22,41 @@ def plot_skew(y):
     plt.title("Skew Plot of Genome")
     plt.xlabel("genome")
     plt.ylabel("#G - #C")
-    plt.show()
+    #plt.show()
     fig.savefig("sequence.png")
 
-if __name__ == "__main__":
-    S = "CATGGGCATCGGCCATACGCC"
+def min_pos(seq):
+    MIN_SEQ = min(seq)
+    for i, v in enumerate(seq):
+        if v == MIN_SEQ:
+            return i , MIN_SEQ
+    return None
+            
+def L_seq_near_min_skew(pos, min_value, L, seq):
+    ''' Return an L length sequence (symmetric)
+        from the genome around min_skew
+        value postion
+    '''
+    return  seq[pos - L//2 : pos + L//2].upper()
 
-    #plot_skew(S)
+def save_result():
+    fname = "./res/result1.txt"
     seq = get_dna_seq().getvalue().lower()
+    positions = skew(seq)
+    pos, min_value = min_pos(positions)
+    plot_skew(positions)
+    L = 500
+    with open(fname, "w") as f:
+        f.write("POSITION: {} \nOF MINIMUM SKEW VALUE: {}\n".format(pos, min_value))
+        f.write("POSSIBLE ORIC REGION :\n{}\n".format(L_seq_near_min_skew(pos, min_value, L, seq)))
+
+if __name__ == "__main__":
+    #plot_skew(S)
+    #seq = get_dna_seq().getvalue().lower()
     #print(seq.lower())
-    print(plot_skew((skew(seq))))
+    #print(plot_skew((skew(seq))))
+    #pos, min_value = min_pos(skew(seq))
+    save_result()
 
     
 
